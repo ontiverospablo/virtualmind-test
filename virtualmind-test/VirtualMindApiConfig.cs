466d234d259267f3577a92b0f6aca1db8d8b887a
Pablo Ontiveros <pablo.ontiveros@gmail.com>
2019-04-25 22:46:49 -0500

    Implemented WebApi configuration

---
+++ virtualmind-test/VirtualMindApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+
+namespace VirtualMindTest
+{
+    public static class VirtualMindApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
